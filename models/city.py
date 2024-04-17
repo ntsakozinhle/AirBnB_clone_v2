0e9d7216bd9cf296b6d183f7d8765b748e0e9eac
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-17 16:31:01 -0700

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -2,14 +2,10 @@
 """ City Module for HBNB project """
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    places = relationship("place", cascade="all, delete", backref="city", passive_deletes=True)
--- models/user.py
+++ models/user.py
@@ -2,15 +2,10 @@
 """This module defines a class User"""
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    places = relationship("Place", cascade="all, delete", backref="user", passive_deletes=True)
